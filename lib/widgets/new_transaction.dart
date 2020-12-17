347a0a2510e7ca68cb0eca6cab7a38663263d462
Ren Weilin <weilinwork99@gmail.com>
2020-12-18 00:17:59 +0800

    Bug fixes

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
+class NewTransaction extends StatefulWidget {
   final Function addTransaction;
 
   NewTransaction(this.addTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final title = titleController.text;
@@ -14,10 +20,11 @@ class NewTransaction extends StatelessWidget {
     if (title.isEmpty || amount <= 0) {
       return;
     }
-    addTransaction(
+    widget.addTransaction(
       title,
       amount,
     );
+    Navigator.of(context).pop();
   }
 
   @override
