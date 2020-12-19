f28cc7da677b3b730707df7e22fe974f48520ee8
Ren Weilin <weilinwork99@gmail.com>
2020-12-20 00:20:34 +0800

    Final Adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction)
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
